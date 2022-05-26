eb6811a817414139749a3dc794ca6372d9e1e684
Serigne Moustapha Sy Ndoye <s.moustapha.sy.ndoye@gmail.com>
2022-05-26 12:30:34 +0000

    Initial Commit

---
+++ elearning-dark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  elearning-dark
+//
+//  Created by S. Moustapha Sy Ndoye on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ elearning-dark/elearning_darkApp.swift
@@ -0,0 +1,17 @@
+//
+//  elearning_darkApp.swift
+//  elearning-dark
+//
+//  Created by S. Moustapha Sy Ndoye on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct elearning_darkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
